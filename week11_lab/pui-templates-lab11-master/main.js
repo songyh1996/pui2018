fa7f281f2dddb70b3e6a2b4399d054845e095735
Antonio Song <songyh1996@gmail.com>
2018-11-29 13:24:37 -0500

    restore

---
+++ week11_lab/pui-templates-lab11-master/main.js
@@ -0,0 +1,42 @@
+// TODO (Step 2): fill in code creating products objects here
+var products = {
+    "cinnamonBuns": [	{
+        				"name": "Birthday Cake",
+           				"price": "$2.00",
+            			"img": "img/birthday-cake.jpg",
+            			"id": 0
+        				},
+
+        				{
+        				"name": "Red Velvet Cake",
+           				"price": "$5.00",
+            			"img": "img/lavender.png",
+            			"id": 0
+        				},
+
+
+        				{
+        				"name": "Maple Walnut Cake",
+           				"price": "$3.00",
+            			"img": "img/maple-walnut.jpg",
+            			"id": 0
+        				}
+        			]
+}
+
+$(document).ready(function(){
+    // TODO (Step 4): fill in code for changing selection-template here
+
+
+    // TODO (Step 7): add function call for 1st update to detail-template
+
+
+    // TODO: (Step 8): add code below for changing product details on click
+
+
+});
+
+// TODO (Step 6): fill in function for updating detail-template based on id
+function updateProductDetail(id) {
+
+}
