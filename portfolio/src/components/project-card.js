164d0479bf025c2e8ad393ce01b4d66a61b2fa48
Antonio Song <songyh1996@gmail.com>
2018-11-30 01:44:05 -0500

    home is working

---
+++ portfolio/src/components/project-card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const ProjectCard = props => (
+	<div className="Project-Card">
+		<img id="project-card-thumbnail" src={props.thumbnail} />
+		<div id="project-card-title"><h3>{props.title}</h3></div>
+		<div id="project-card-logo-container"><img id="project-card-logo" src={props.logo} /></div>
+		<p class="Project-Card-Description">{props.text}</p>
+		<Link to={props.link}>View Project →</Link>
+	</div>
+)
+
+export default ProjectCard
--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import ProjectCard from '../components/project-card'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -9,11 +10,25 @@ const IndexPage = () => (
   	<div className="Hero">
   		<div className="HeroGroup">
   			<h1>Carnegie Mellon University '20 <br/> Information Systems + Human-Computer Interaction <br/>User Experience Designer</h1>
-   			<h2>Previously at PayPal, Venmo, and Apple</h2>
+   			<h2>Previously @PayPal, Venmo, and Apple.</h2>
   		</div>
   	</div>
-  	<div className="Project">
-  		<div className="ProjectGroup">
+  	<div className="Projects">
+  		<div className="ProjectsGroup">
+  			<ProjectCard 
+  				thumbnail={require('../images/my-benefits-thumbnail.png')}
+  				title="My Benefits"
+  				logo={require('../images/paypal-logo.png')}
+  				text="My Benefits offer a comprehensive and easy-to-use dashboard that summarizes all available PayPal benefits for the users."
+  				link={'/my-benefits'}
+  			/>
+  			<ProjectCard 
+  				thumbnail={require('../images/my-benefits-thumbnail.png')}
+  				title="CubeRover TeleOperation Interface"
+  				logo={require('../images/paypal-logo.png')}
+  				text="My Benefits offer a comprehensive and easy-to-use dashboard that summarizes all available PayPal benefits for the users."
+  				link={'/my-benefits'}
+  			/>
   		</div>
   	</div>
   </Layout>
