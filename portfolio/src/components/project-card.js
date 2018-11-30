a79323d17a331d82bf25a34e15b2ec7b9e17d100
Antonio Song <songyh1996@gmail.com>
2018-11-30 10:43:02 -0500

    debugging

--- portfolio/src/components/project-card.js
+++ portfolio/src/components/project-card.js
@@ -5,7 +5,7 @@ import { Link } from 'gatsby'
 
 const ProjectCard = props => (
 	<div className="Project-Card">
-		<img id="project-card-thumbnail" src={props.thumbnail} alt="project thumbnail"/>
+		<Link to={props.link}><img id="project-card-thumbnail" src={props.thumbnail} alt="project thumbnail"/></Link>
 		<div id="project-card-title"><h3>{props.title}</h3></div>
 		<div id="project-card-logo-container"><img id="project-card-logo" src={props.logo} alt="project logo"/></div>
 		<p className="Project-Card-Description">{props.text}</p>
