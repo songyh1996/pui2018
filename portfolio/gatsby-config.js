ce12b87c7548eb6cfaab6c0fa9dee09af8d6bedc
Antonio Song <songyh1996@gmail.com>
2018-11-30 23:42:31 -0500

    hello

--- portfolio/gatsby-config.js
+++ portfolio/gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Antonio Song | User Experience Designer',
   },
+  pathPrefix: "/portfolio",
   plugins: [
     'gatsby-plugin-react-helmet',
     {
